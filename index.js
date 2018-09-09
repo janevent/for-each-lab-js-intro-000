e0464c686a49f2d7be9ac67faf75851d7109a2c1
Jane Venter <janeanneventer@gmail.com>
2018-09-09 16:51:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function iterativeLog(array){
+console.log(`${index): ${element}`);
+}
+array.forEach(interativeLog);
+
+function iterate(callback){
+  var array = ['shoes', 'socks', 'dress'];
+  array.forEach(callback);
+  return array;
+}
+doToArray(array, callback){
+  array.forEach(callback);
+}
+165.277.31.156.34870
